d06114ac69fb79ad641b9ee78958cccd8019269a
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-08-01 22:12:16 -0500

    added components

--- micro-blog/src/Components/Home.js
+++ micro-blog/src/Components/Home.js
@@ -1,13 +1,23 @@
+import {useState} from "react";
+
 const Home = () => {
 
-    const handleClick = () => {
-        console.log("yo")
-    }
+    const [blogs, setBlogs] = useState([
+        {title: 'my new website', body: 'lorem', author: 'mario' , id: 1},
+        {title: 'my party', body: 'lorem', author: 'luigi' , id: 2},
+        {title: 'im kidnapped', body: 'lorem', author: 'peach' , id: 3},
+
+    ]);
+
 
     return(
  <div className="Home">
-     <h2>Homepage</h2>
-     <button onClick={handleClick}>Click me</button>
+     {blogs.map((blog)=> (
+         <div className={"blog-preview"} key={blog.id}>
+             <h2>{blog.title}</h2>
+             <p>Written by: {blog.author}</p>
+         </div>
+     ))}
  </div>
     )
 }
