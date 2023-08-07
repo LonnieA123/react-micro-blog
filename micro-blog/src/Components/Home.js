3fe22081fcbbe5e3cbc9b4ba5b980806df9f2fad
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-08-07 13:24:40 -0500

    error handling for our home page

--- micro-blog/src/Components/Home.js
+++ micro-blog/src/Components/Home.js
@@ -4,7 +4,8 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
-
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null)
     const title = "Welcome to our blog"
 
 
@@ -12,16 +13,27 @@ const Home = () => {
     useEffect(() => {
         fetch('http://localhost:8001/blogs')
             .then(res => {
+                if (!res.ok){
+                    throw Error('could not fetch data')
+                }
                 return res.json()
             })
             .then(data => {
                 setBlogs(data)
+                setIsLoading(false)
+                setError(null)
+            })
+            .catch(err => {
+                setIsLoading(false)
+                setError(err.message)
             })
     },[])
 
     return(
  <div className="Home">
-     {blogs && <BlogList blogs={blogs} title={title} />}
+     { error && <div> { error } </div>}
+     { isLoading && <div>Loading...</div>}
+     { blogs && <BlogList blogs={blogs} title={title} />}
  </div>
     )
 }
