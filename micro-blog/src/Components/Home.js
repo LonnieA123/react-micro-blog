57755f88952c01df34b8a9f85c550af721dffb81
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-08-07 14:05:38 -0500

    created useFetch hook

--- micro-blog/src/Components/Home.js
+++ micro-blog/src/Components/Home.js
@@ -1,39 +1,17 @@
 import {useEffect, useState} from "react";
 import BlogList from "./BlogList";
+import {useFetch} from "../Hooks/useFetch";
 
 const Home = () => {
+    const {data, isLoading, error } = useFetch('http://localhost:8001/blogs')
 
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState(null)
     const title = "Welcome to our blog"
-
-
-
-    useEffect(() => {
-        fetch('http://localhost:8001/blogs')
-            .then(res => {
-                if (!res.ok){
-                    throw Error('could not fetch data')
-                }
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data)
-                setIsLoading(false)
-                setError(null)
-            })
-            .catch(err => {
-                setIsLoading(false)
-                setError(err.message)
-            })
-    },[])
 
     return(
  <div className="Home">
      { error && <div> { error } </div>}
      { isLoading && <div>Loading...</div>}
-     { blogs && <BlogList blogs={blogs} title={title} />}
+     { data && <BlogList blogs={data} title={title} />}
  </div>
     )
 }
