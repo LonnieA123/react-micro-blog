a745b1b302b9b22c93b49686918ac773e7126290
LonnieA123 <lonnie.j.arellano@gmail.com>
2023-08-10 23:39:34 -0500

    created routes

--- micro-blog/src/App.js
+++ micro-blog/src/App.js
@@ -2,11 +2,12 @@ import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+
+//route for home
 function App() {
   return (
     <div className="App">
         <Navbar/>
-
         <div className="content">
             <Switch>
                 <Route path="/">
